79f4e3d7aaa1769fa511296ed9b5ff2bcb0ee8a7
Pedro Furtado <furtado.pedropaulo@gmail.com>
2022-09-19 00:25:06 -0300

    adding  query

--- src/main/java/com/ppfurtado/spring5recipeapp/repository/CategoryRepository.java
+++ src/main/java/com/ppfurtado/spring5recipeapp/repository/CategoryRepository.java
@@ -3,5 +3,9 @@ package com.ppfurtado.spring5recipeapp.repository;
 import com.ppfurtado.spring5recipeapp.domain.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    Optional<Category> findByDescription(String description);
 }
--- src/main/java/com/ppfurtado/spring5recipeapp/repository/UnitOfMeasureRepository.java
+++ src/main/java/com/ppfurtado/spring5recipeapp/repository/UnitOfMeasureRepository.java
@@ -4,5 +4,10 @@ import com.ppfurtado.spring5recipeapp.domain.Category;
 import com.ppfurtado.spring5recipeapp.domain.UnitOfMeasure;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends JpaRepository<UnitOfMeasure , Long> {
+
+
+    Optional<UnitOfMeasure> findByDescription(String description);
 }
