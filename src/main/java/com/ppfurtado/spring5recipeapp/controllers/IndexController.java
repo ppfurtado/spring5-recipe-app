6d9aea8817c348c7ef719e34f77536f5eb4578e7
Pedro Furtado <pedro.nazare@pdcase.com.br>
2022-09-24 00:12:12 -0300

    refactor get recipes

--- src/main/java/com/ppfurtado/spring5recipeapp/controllers/IndexController.java
+++ src/main/java/com/ppfurtado/spring5recipeapp/controllers/IndexController.java
@@ -1,32 +1,28 @@
 package com.ppfurtado.spring5recipeapp.controllers;
 
-import com.ppfurtado.spring5recipeapp.domain.Category;
-import com.ppfurtado.spring5recipeapp.domain.UnitOfMeasure;
-import com.ppfurtado.spring5recipeapp.repository.UnitOfMeasureRepository;
-import com.ppfurtado.spring5recipeapp.repository.CategoryRepository;
+import com.ppfurtado.spring5recipeapp.domain.Recipe;
+import com.ppfurtado.spring5recipeapp.services.RecipeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.Optional;
+import java.util.Set;
 
 @Controller
 public class IndexController {
 
-    private CategoryRepository categoryRepository;
-    private UnitOfMeasureRepository unitOfMeasureRepository;
+    private final RecipeService recipeService;
 
-    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
-        this.categoryRepository = categoryRepository;
-        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    public IndexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
     }
 
     @RequestMapping({"","/","/index"})
-    public String getIndexPage(){
+    public String getIndexPage(Model model){
 
-        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
-        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
-        categoryOptional.ifPresent(category -> System.out.println("Cat Id is: " + category.getId()));
-        unitOfMeasureOptional.ifPresent(uom -> System.out.println("Uom Id is: " + uom.getId()));
+        Set<Recipe> recipes = recipeService.getRecipe();
+
+        model.addAttribute("recipes", recipes);
 
         return "index";
     }
